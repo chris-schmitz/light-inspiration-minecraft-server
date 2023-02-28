2b68bddfc8306a9f93ff3084c731621c53fa4b94
Christopher Schmitz <schmitz.chris@gmail.com>
2023-02-27 22:02:25 -0600

    Breaks out tests for directory builder

---
+++ infrastructure/scripts/test/test_directory_builder.py
@@ -0,0 +1,29 @@
+import shutil
+from os import makedirs, path
+
+import pytest
+
+from directory_builder import DirectoryBuilder
+
+
+class TestRunner:
+    _ROOT_OF_TARGET_DIRECTORY = "./delete_this"
+    TARGET_DIRECTORY = f"{_ROOT_OF_TARGET_DIRECTORY}/test/directory"
+
+    @pytest.fixture(autouse=True)
+    def run_after_each_test(self):
+        yield  # * yield says "pause while the tests run, so everything after yield is after a tests * #
+        shutil.rmtree(self._ROOT_OF_TARGET_DIRECTORY)
+
+    def test_can_build_minecraft_directory_structure(self):
+        builder = DirectoryBuilder(makedirs)
+
+        builder.build_directory_structure(self.TARGET_DIRECTORY)
+        path.exists(self.TARGET_DIRECTORY)
+
+    def test_if_directory_already_exists_the_directory_already_exists_exception_is_suppressed(self):
+        builder = DirectoryBuilder(makedirs)
+        makedirs(self.TARGET_DIRECTORY)
+
+        builder.build_directory_structure(self.TARGET_DIRECTORY)
+        path.exists(self.TARGET_DIRECTORY)
